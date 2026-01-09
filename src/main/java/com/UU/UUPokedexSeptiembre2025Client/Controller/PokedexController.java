8d841324179c4dd68ca37eed56e9c3c3a66b85ee
digis <digis@192.167.0.112>
2026-01-09 10:48:32 -0600

    pokedexController

---
+++ src/main/java/com/UU/UUPokedexSeptiembre2025Client/Controller/PokedexController.java
@@ -0,0 +1,14 @@
+package com.UU.UUPokedexSeptiembre2025Client.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("pokedex")
+public class PokedexController {
+    
+    
+    
+        
+    
+}

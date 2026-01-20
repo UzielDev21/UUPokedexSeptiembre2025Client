5241c0f6d3284f1809bea70f9370a465468545b9
digis <digis@ALIEN27>
2026-01-19 18:40:14 -0600

    Integración de pantalla de carga

---
+++ src/main/java/com/UU/UUPokedexSeptiembre2025Client/ML/Roles.java
@@ -0,0 +1,32 @@
+package com.UU.UUPokedexSeptiembre2025Client.ML;
+
+public class Roles {
+
+    private int role_Id;
+    private String role_Name;
+
+    public Roles() {
+    }
+
+    public Roles(int role_Id, String role_Name) {
+        this.role_Id = role_Id;
+        this.role_Name = role_Name;
+    }
+
+    public int getRole_Id() {
+        return role_Id;
+    }
+
+    public void setRole_Id(int role_Id) {
+        this.role_Id = role_Id;
+    }
+
+    public String getRole_Name() {
+        return role_Name;
+    }
+
+    public void setRole_Name(int role_Id) {
+        this.role_Id = role_Id;
+    }
+
+}
